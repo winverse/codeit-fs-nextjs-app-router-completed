2c47f755ecb16b056c036122965117cb051b8ee5
winverse <public.winverse@gmail.com>
2026-09-04 18:05:48 +0900

    style(course-27): ESLint 설정 경로를 표기한다

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint.config.mjs
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import prettier from "eslint-config-prettier/flat";
